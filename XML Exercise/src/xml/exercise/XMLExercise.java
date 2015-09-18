8acac8b10e4bd34aa5021f262d13d1e52066bb89
Knappmann <7h380x@live.dk>
2015-09-18 11:39:54 +0200

    start

---
+++ XML Exercise/src/xml/exercise/XMLExercise.java
@@ -0,0 +1,17 @@
+
+package xml.exercise;
+
+/**
+ *
+ * @author Patrick
+ */
+public class XMLExercise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
